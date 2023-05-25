c1ca3774f8885b343dd5ec369932a7bb557acc44
Marcus Little <marcuslittle@marcuss-mbp-2.lan>
2023-05-24 23:43:04 -0400

    add gatsby-react-helmet

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,11 +1,18 @@
 import * as React from "react"
 import { Link } from "gatsby"
+import { Helmet } from 'react-helmet'
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
 const SecondPage = () => (
   <Layout>
+    <Helmet>
+      <title>Test Title!</title>
+      <meta name="description" content="test1" />
+      <meta property="og:title" content="test2" />
+      <meta property="og:description" content="test3" />
+    </Helmet>
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
